59132448c99233178cc67b078dc8ab1760530011
+ <+>
2025-02-05 20:34:42 +0300

    Sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print([0, 1])
+    else:
+        fibonacci = [0, 1]
+        while len(fibonacci) < length:
+            fibonacci.append(fibonacci[-1] + fibonacci[-2])
+        print(fibonacci)
